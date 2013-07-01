d67f6ae4a23822224d8e0704ea8e1b6b5984d3e0
Kiril Vladimirov <kiril@vladimiroff.org>
2013-07-01 10:27:11 +0300

    Update @vstoykov's convert_encoding.py

--- bin/convert_encoding.py
+++ bin/convert_encoding.py
@@ -1,48 +1,48 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # coding: utf-8
 """
 Program that convert text files from one encoding to another.
-By default it is used to convert windows-1251 encoded subtitles
-into ISO-8859-5 ecoded because this is encoding for cyrilic
-characters in Panasonic Viera TV
+By default it is used to convert windows-1251 to UTF-8.
+Tested on Python 2.7+ and Python 3.2+
 
-created by Venelin Stoykov <vkstoykov@gmail.com>
+Created by Venelin Stoykov <vkstoykov@gmail.com>
 """
+from __future__ import unicode_literals
 import sys
 import os
 from optparse import make_option, OptionParser
 
-__version__ = (0, 2)
+__version__ = (0, 4)
 
 DEFAULT_INPUT_ENCODING = 'windows-1251'
-DEFAULT_OUTPUT_ENCODING = 'iso-8859-5'
+DEFAULT_OUTPUT_ENCODING = 'utf-8'
 
 FAILSAFE_CHARACTERS = {
-    u'\u2122': 'TM',
-    u'\u201c': '"',
-    u'\u201d': '"',
+    '\u2122': 'TM',
+    '\u201c': '"',
+    '\u201d': '"',
+    '\u2026': '...',
 }
 
 
 def get_version():
-    return '.'.join(unicode(x) for x in __version__)
+    return '.'.join(str(x) for x in __version__)
 
 
 def get_failsafe_char(char, output_encoding):
     return FAILSAFE_CHARACTERS.get(char, '?').encode(output_encoding)
 
 
-def convert_to(in_file_name,
-               input_encoding=DEFAULT_INPUT_ENCODING,
+def convert_to(in_file_name, input_encoding=DEFAULT_INPUT_ENCODING,
                output_encoding=DEFAULT_OUTPUT_ENCODING):
     out_file_name = "%s.%s%s" % (in_file_name[:-4],
                                  output_encoding, in_file_name[-4:])
 
-    with open(in_file_name, 'r') as in_file:
+    with open(in_file_name, 'rb') as in_file:
         try:
             content = in_file.read().decode(input_encoding)
         except Exception as ex:
-            print "Can't read '%s' because: %s" % (in_file_name, ex)
+            print("Can't read '%s' because: %s" % (in_file_name, ex))
         else:
             new_content = []
             for char in content:
@@ -51,7 +51,7 @@ def convert_to(in_file_name,
                 except Exception as ex:
                     new_content.append(get_failsafe_char(char, output_encoding))
 
-            with open(out_file_name, 'w') as out_file:
+            with open(out_file_name, 'wb') as out_file:
                 for char in new_content:
                     out_file.write(char)
 
@@ -65,7 +65,8 @@ if __name__ == '__main__':
     prog_name = os.path.basename(sys.argv[0])
     opt_parser = OptionParser(
         prog=prog_name,
-        version=get_version(),
+        version="%prog " + get_version(),
+        usage='usage: %prog [options] file1 [[file2] ... [fileN]]',
         option_list=(
             make_option('-i', '--input-encoding', dest='input_encoding',
                         default=DEFAULT_INPUT_ENCODING,
