e3a43fb8676da555bc05bf50c3d60011f3695418
Kiril Vladimirov <kiril@vladimiroff.org>
2013-09-23 11:57:23 +0300

    @barrtin made me do that

---
+++ bin/winnie_the_pooh.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+echo '
+                              XXXX
+                            XX    XX
+         XXXX              XX        XX
+       XXX   XX           XX          XX
+     XX        XX  XXXX  XX           XX
+    XX           XX    XXXX           XX
+   XX             XX       XXX        XX
+   XX             XX         XXX     XX
+    XX                         XXX  XX
+     XX                          XXX
+      XX                           XX
+       XX                           XX
+        XXXX                          XX
+      XX                                XX
+      XX                                  XX
+      XX                              X    XX
+     XX                             XXX    XX
+     XX                            XX      XX
+    XXX                               XX   XX
+    XXX                             XXXX   XX
+    XXX                             XXX    XX
+    XXX                                     XXXX
+    XXX                                        XXXXX
+     XX                                        XXXXX
+      XX                                        XXXX
+XX    XX                                        XXX
+ XXXXXXXX                         X            XX
+ XX.....XXXXX                    XX           XX
+   XX.......XXXXXXX            XX XX         XX
+    XX............XXXXXXX       XX  XX     XXX
+     XX..X..............XXXXXX    XXXXXXXXXX
+      XXX....................XXXX     XX
+      XX........................XXXX XXXXX
+     XX.............................XXXX..XX
+    XX.................................XX..XX
+   XX......................................XX
+  XXX.......................................XX
+ XXXX.........................................XX
+ XXX...................X........................XX
+ XXXXXXX...............X...........................XX
+  XX    XXXX...........XX..........................XX
+  XX        XXX.......XX............................XX
+  XX           XXX...XX..............................XX
+  XX              XXXX....XXXXXXXXXXXXXXXXXX..........XX
+  XX               XXXXXXXX                XXXXXXXXXXXXXX
+  XX              XX                                  XX
+  XX             XX                                    XX
+ XX              XX                                      XX
+ XX             XX                                        XX
+ XX             XX                                         XX
+ XX             XX                                          XX
+ XX             XX                                            XX
+ XX             XX                                             XX
+ XX             XX                                              X XXX
+  XX            XX                                              XX   XX
+  XX            XX                                               XX     XX
+  XX           XX                                                 XX       XX
+  XX           XX                                                 XX        XX
+  XX          XX                                                   XX       XX
+  XX         XX                                                    XX       XX
+   XX       XX   X                                                XX       XX
+    XX    XXX   XX               X                                XXXX    XX
+      XXXX  XX  XX                XX                              XX XXXXXX
+              XX XX                  XXX                          XX
+              XXXX                     XXX                       XX
+                XX                       XXX                    XX
+                XX                          XX                 XX
+                XX                          XX               XX
+                 XX                           XX            XX
+                XXXX                           XX         XX
+             XXX   XX                           XX     XXX
+           XX       XX                          XX  XXX
+         XX           XX                       XXXXXXX
+        XX              XX                  XXX       XXX
+       XX                 XX              XXX    XXX     XX
+       XX                   XXX          XX   XXX        XX
+        XX                     XX       XX   XX          XX
+        XX                  XXX XX     XX   XX            XX
+         XX            XXXXX      XX  XX   XX             XX
+         XX          XX  XX        XXXX    XX            XX
+          XX        X     XX        XXX   XX             XX
+           XX              XX        XX   XX             XX
+            XX              XX       XX    XX           XX
+             XX              XX       XX   XX           XX
+               XX            XX       XX    XX         XX
+                 XX          XX        XX   XX        XX
+                  XXX     XXX          XX          XXX
+                      XXXXX              XXX     XXX
+                                           XXXXX
+'
